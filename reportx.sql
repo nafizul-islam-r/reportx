06abb0a0f93b1dc9fb62ce32e64ca691b8519467
nafizul-islam-r <smnafizulislam@gmail.com>
2025-05-26 23:53:29 +0600

    db modified

--- reportx.sql
+++ reportx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 26, 2025 at 07:46 PM
+-- Generation Time: May 26, 2025 at 07:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `reportx`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scam_reports`
+--
+
+CREATE TABLE `scam_reports` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `scam_type` enum('Online','Offline') NOT NULL,
+  `scammer_name` varchar(100) NOT NULL,
+  `scammer_info` text NOT NULL,
+  `description` text NOT NULL,
+  `status` enum('Pending','Reported','Solved') NOT NULL DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,6 +64,13 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `scam_reports`
+--
+ALTER TABLE `scam_reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -57,10 +81,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `scam_reports`
+--
+ALTER TABLE `scam_reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `scam_reports`
+--
+ALTER TABLE `scam_reports`
+  ADD CONSTRAINT `scam_reports_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
